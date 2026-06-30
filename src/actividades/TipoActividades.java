dcc346a5c40191fd5d1c683db9a34574abb88a22
joanrecioj <joamreciojimenez@gmail.com>
2026-06-29 22:27:26 -0600

    Se crea el enum de actividades con diferentes tipos segun el enunciado del hotel

---
+++ src/actividades/TipoActividades.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package actividades;
+
+/**
+ *
+ * @author joanreciojimenez
+ */
+public enum TipoActividades {
+    CAMINATAS,   
+    CLASES_DE_COCINA,
+    RECORRIDOS_TURISTICOS;
+}
