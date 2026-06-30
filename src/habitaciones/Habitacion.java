1ac1e840c67b10d6a525c6de8c1ab5ead065073a
BrandonXdev <brchevezac@est.utn.ac.cr>
2026-06-29 20:51:19 -0600

    Clase Alojamiento
    Se modifica la clase Padre Alojamiento, se agrega metodo to string

---
+++ src/habitaciones/Habitacion.java
@@ -0,0 +1,24 @@
+
+package habitaciones;
+import alojamientos.Alojamiento; 
+
+
+public class Habitacion extends Alojamiento{
+    private int numeroPiso;
+    private boolean balcon;
+
+    public int getNumeroPiso() {
+        return numeroPiso;
+    }
+
+    public boolean isBalcon() {
+        return balcon;
+    }
+
+    public Habitacion(int numeroPiso, boolean balcon, int codigo, String nombre,
+            int capacidadMaxima, int precioPorNoche, boolean disponible) {
+        super(codigo, nombre, capacidadMaxima, precioPorNoche, disponible);
+        this.numeroPiso = numeroPiso;
+        this.balcon = balcon;
+    }
+}
