87a09979a5cdc2ac1d7935af5e17adeb9d270140
BrandonXdev <brchevezac@est.utn.ac.cr>
2026-06-29 22:01:23 -0600

    Clase Habitacion
    Se crea la clase habitacion heredando de alojamiento, con sus metodos de acceso, constructor, to string usando super.toString para utilizar el to string de la clase padre

---
+++ src/habitaciones/Habitacion.java
@@ -0,0 +1,30 @@
+
+package habitaciones;
+import alojamientos.Alojamiento; 
+
+
+public class Habitacion extends Alojamiento{
+    private int numeroPiso;
+    private boolean balcon;
+
+    public int getNumeroPiso() {
+        return numeroPiso;
+    }
+
+    public boolean isBalcon() {
+        return balcon;
+    }
+
+    public Habitacion(int numeroPiso, boolean balcon, int codigo, String nombre,
+            int capacidadMaxima, int precioPorNoche, boolean disponible) {
+        super(codigo, nombre, capacidadMaxima, precioPorNoche, disponible);
+        this.numeroPiso = numeroPiso;
+        this.balcon = balcon;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+         "Numero de piso: " + numeroPiso + "\nBalcon: " + balcon;
+    }
+}
