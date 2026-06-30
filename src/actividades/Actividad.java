642ed062d0c6134a4b17bc8aae80f5f58798a38e
BrandonXdev <brchevezac@est.utn.ac.cr>
2026-06-29 22:31:50 -0600

    Clase Actividad
    Se crea la clase actividad con sus metodos de acceso, constructor y metodos..

--- src/actividades/Actividad.java
+++ src/actividades/Actividad.java
@@ -4,10 +4,93 @@
  */
 package actividades;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import personas.Cliente;
+
 /**
  *
  * @author brand
  */
 public class Actividad {
+    private String nombre;
+    private LocalDate fecha;
+    private LocalTime hora;
+    private int cupoMaximo;
+    private Cliente[] clientes;
+    private TipoActividad tipo;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public int getCupoMaximo() {
+        return cupoMaximo;
+    }
+
+    public Cliente[] getClientes() {
+        return clientes;
+    }
+
+    public Actividad(String nombre, LocalDate fecha, LocalTime hora, int cupoMaximo, TipoActividad tipo) {
+        this.nombre = nombre;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.cupoMaximo = cupoMaximo;
+        this.clientes = new Cliente[cupoMaximo];
+        this.tipo = tipo;
+    }
+    
+    public boolean cuposDisponibles(){
+        for (int i = 0; i < clientes.length; i++) {
+            if (clientes[i] != null) {
+                return true;   
+            }
+        }
+        return false;
+    }
+    
+    public void inscribirClientes(Cliente cliente){
+        for (int i = 0; i < clientes.length; i++) {
+            if (clientes[i] == null) {
+                clientes[i] = cliente;       
+            }
+        }
+    }
+    
+    public double porcentajeOcupacion(){
+        int ocupados = 0;
+        for (int i = 0; i < clientes.length; i++) {
+            if (clientes[i] != null) {
+                ocupados++;
+                
+            }
+        }
+        if (cupoMaximo == 0) {
+            return 0;
+        }
+        return (ocupados * cupoMaximo) / 100;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
+    
+    
+    
     
 }
